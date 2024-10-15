74124f1f0f9bc7bdc26436e5c6115e0b6235cc8f
zhengdao <316321454@qq.com>
2024-10-16 00:00:14 +0200

    [DEV] First onchain part - Contrat

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,12 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-contract Collection {
-  string public name;
-  int public cardCount;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-  constructor(string memory _name, int _cardCount) {
-    name = _name;
+
+contract Collection is ERC721,Ownable{
+
+  string public collectionName;
+  uint256 public cardCount;
+
+//les tokens pour les cards de chaque collection doivent être uniques
+  uint256 private counter_token_ids = 0;
+
+  struct CardMetaData{
+    uint256 cardNumber;
+    string ImgField;
+  }
+
+  mapping(uint256 => CardMetaData) private CollectionCards;
+
+  constructor(address user,string memory _name, uint256 _cardCount) Ownable(user) ERC721(_name, "Collection"){
+    collectionName = _name;
     cardCount = _cardCount;
   }
+
+  //generer un card
+  function mintCard(address recipient,uint256 collection_id,uint256 cardNumber,string memory ImgField) public onlyOwner returns (uint256){
+    uint256 token_ids = counter_token_ids++;
+    CollectionCards[token_ids] = CardMetaData(cardNumber,ImgField);
+    //d'abord on met tous les collections le meme adresse que le Main contract
+    //alors pour eviter les conflits de token_id on fait une formule avec le facteur collection_id pour generer un unique_id
+    uint256 unique_id = collection_id * 1e6 + token_ids;
+    _mint(recipient, token_ids);
+    return unique_id;
+  }
+
+  function getCollectionCards(uint256 token_id) public view returns (CardMetaData memory){
+    //check if the token_id exists dans la CollectionCards
+    require(token_id <= counter_token_ids, "ERROR: Le token_id n'existe pas dans la CollectionCards");
+    return CollectionCards[token_id];
+  }
+
+
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -3,15 +3,40 @@ pragma solidity ^0.8;
 
 import "./Collection.sol";
 
-contract Main {
-  int private count;
-  mapping(int => Collection) private collections;
+contract Main is Ownable {
+  uint256 private count;
+  mapping(uint256 => Collection) private collections;
 
-  constructor() {
+  //on set events ici pour communiquer avec le front end
+  event CollectionCreated(uint256 count, string name, uint256 cardCount);
+  event CardMinted(uint256 collectionID, uint256 unique_id, address recipient, uint256 cardNumber, string ImgField);
+
+  constructor(address owner) Ownable(owner) {
     count = 0;
   }
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
+  function createCollection(string calldata name, uint256 cardCount) external onlyOwner{
+    address user =  address (this);
+    Collection newCollection = new Collection(user,name, cardCount);
+    collections[count++] = newCollection;
+
+    //emit un event? a voir
+    emit CollectionCreated(count, name, cardCount);
   }
+
+  function mintCard(uint256 collectionID, address recipient, uint256 cardNumber, string calldata ImgField) external onlyOwner {
+    require(collectionID <= count, "ERROR: Collection does not exist");
+    uint256 unique_id = collections[collectionID].mintCard(recipient, collectionID,cardNumber, ImgField);
+    //pour tracker les cards minted
+    emit CardMinted(collectionID, unique_id,recipient, cardNumber, ImgField);
+  }
+
+  function getCollection(uint256 collectionID) public returns (Collection) {
+    return collections[collectionID];
+  }
+
+  function getCollectionCount() public view returns (uint256) {
+    return count;
+  }
+
 }
